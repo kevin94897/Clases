c69932095b17d95d258e5186a1b346403d82f0b0
jmayta17@hotmail.com <Monica$2017>
2019-03-02 18:08:01 -0500

    Ejercicio RecyclerView

--- Inventory/app/src/main/java/pe/edu/cibertec/inventory/MainActivity.java
+++ Inventory/app/src/main/java/pe/edu/cibertec/inventory/MainActivity.java
@@ -1,9 +1,13 @@
 package pe.edu.cibertec.inventory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -53,7 +57,19 @@ public class MainActivity extends AppCompatActivity {
         Product productMouse = new Product("Mouse",
                 "Modelo Genious", 122);
         items.add(productMouse);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(this, ProductActivity.class);
+        startActivity(intent);
+        return true;
     }
 }
